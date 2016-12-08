8ac05af43d57390c919a3519ed47bffb8c9cc6d7
almutair018 <aool-1@hotmail.com>
2016-12-08 00:25:06 -0500

    this is the end of lesson02

---
+++ src/app/boot.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+import {bootstrap} from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-root',
+  template: '<h1>Welcome to my App</h1>'
+})
+
+class AppComponent {
+
+}
+
+bootstrap(AppComponent);
