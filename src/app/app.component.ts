d3100ff4d2c07fdf558cdd1f06b8cce36f7e91c9
almutair018 <aool-1@hotmail.com>
2016-12-08 00:37:51 -0500

    this is the end of lesson04

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,24 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 
 })
 
 export class AppComponent {
+  name: string;
+  artists: any;
 
+  constructor() {
+    this.name =  'Ray';
+    this.artists = [
+      {
+          name: 'Barot Bellingham',
+          school: 'Penn State'
+      },{
+          name: 'Hillary Post',
+          school: 'University of Illinois'
+      }
+    ];
+  }
 }
